5fc832c26aea11c66f93a7fa5ca3c1f917b87072
efraintinoco <oakcliff_21@yahoo.com>
2022-07-12 14:29:23 -0500

    added all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/contact/admin', pathMatch: 'full' },
+  { path: 'contacts/admin', component: ContactManagerComponent },
+  { path: 'contacts/add', component: AddContactComponent},
+  { path: 'contacts/edit/:contactID', component: EditContactComponent },
+  { path: 'contacts/view/contactID', component: ViewContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
